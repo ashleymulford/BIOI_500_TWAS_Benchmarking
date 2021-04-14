e8a04e5e11bd1da1515cd08f5e169d3f4b8848d5
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2021-04-14 15:11:05 -0500

    Update fusion.sh

--- fusion.sh
+++ fusion.sh
@@ -8,7 +8,7 @@ Rscript /homes/amulford/fusion_twas-master/FUSION.assoc_test.R \
 --out /homes/amulford/fusion_output/uc_Whole_Blood_1_assoc.txt
 
 
-#Ulcerative Colitis Phenotype
+#Ulcerative Colitis Phenotype:
 
 declare -a tissues=("Adipose_Subcutaneous" "Adipose_Visceral_Omentum" "Adrenal_Gland" "Artery_Aorta" "Artery_Coronary" "Artery_Tibial" "Brain_Amygdala" "Brain_Anterior_cingulate_cortex_BA24" "Brain_Caudate_basal_ganglia" "Brain_Cerebellar_Hemisphere" "Brain_Cerebellum" "Brain_Cortex" "Brain_Frontal_Cortex_BA9" "Brain_Hippocampus" "Brain_Hypothalamus" "Brain_Nucleus_accumbens_basal_ganglia" "Brain_Putamen_basal_ganglia" "Brain_Spinal_cord_cervical_c-1" "Brain_Substantia_nigra" "Breast_Mammary_Tissue" "Cells_Cultured_fibroblasts" "Cells_EBV-transformed_lymphocytes" "Colon_Sigmoid" "Colon_Transverse" "Esophagus_Gastroesophageal_Junction" "Esophagus_Mucosa" "Esophagus_Muscularis" "Heart_Atrial_Appendage" "Heart_Left_Ventricle" "Liver" "Lung" "Minor_Salivary_Gland" "Muscle_Skeletal" "Nerve_Tibial" "Ovary" "Pancreas" "Pituitary" "Prostate" "Skin_Not_Sun_Exposed_Suprapubic" "Skin_Sun_Exposed_Lower_leg" "Small_Intestine_Terminal_Ileum" "Spleen" "Stomach" "Testis" "Thyroid" "Uterus" "Vagina" "Whole_Blood")
 
@@ -26,11 +26,11 @@ for chr in {1..22}
 done
 
 
-#Cholesterol Phenotype
+#Cholesterol Phenotype:
 
-#Asthma Phenotype
+#Asthma Phenotype:
 
-#COVID-19 Phenotype
+#COVID-19 Phenotype:
 
 
 
