82ca59bde349a74bd03bb18c8339d4afe1ea76e7
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2021-04-14 16:03:52 -0500

    Update format_fusion_sum_stats_input.sh

--- format_fusion_sum_stats_input.sh
+++ format_fusion_sum_stats_input.sh
@@ -1,5 +1,6 @@
 #uc gwas sum stats to fusion input
 awk '{print $3 FS $4 FS $5 FS $7/($8+0.001)}' uc_anderson_2011_21297633_uc_efo0000729_1_gwas.sumstats.tsv > uc_fusion_anderson_2011_21297633_uc_efo0000729_1_gwas.sumstats.tsv
+#must have column names: SNP, A1, A2, Z
 
 #chol
 
