479d7c6f201fadec25b1065483ed8cf8086fe369
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2021-04-14 15:53:20 -0500

    Update format_fusion_sum_stats_input.sh

--- format_fusion_sum_stats_input.sh
+++ format_fusion_sum_stats_input.sh
@@ -1,3 +1,8 @@
 #uc gwas sum stats to fusion input
-
 awk '{print $3 FS $4 FS $5 FS $7/($8+0.001)}' uc_anderson_2011_21297633_uc_efo0000729_1_gwas.sumstats.tsv > uc_fusion_anderson_2011_21297633_uc_efo0000729_1_gwas.sumstats.tsv
+
+#chol
+
+#asthma
+
+#covid-19
