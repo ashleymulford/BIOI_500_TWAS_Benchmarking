a319b8fe26171cc512bd8ad739a13d81ee5f7df4
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2021-04-14 15:34:22 -0500

    Update fusion.sh

--- fusion.sh
+++ fusion.sh
@@ -11,13 +11,12 @@ Rscript /homes/amulford/fusion_twas-master/FUSION.assoc_test.R \
 #Ulcerative Colitis Phenotype:
 
 declare -a tissues=("Adipose_Subcutaneous" "Adipose_Visceral_Omentum" "Adrenal_Gland" "Artery_Aorta" "Artery_Coronary" "Artery_Tibial" "Brain_Amygdala" "Brain_Anterior_cingulate_cortex_BA24" "Brain_Caudate_basal_ganglia" "Brain_Cerebellar_Hemisphere" "Brain_Cerebellum" "Brain_Cortex" "Brain_Frontal_Cortex_BA9" "Brain_Hippocampus" "Brain_Hypothalamus" "Brain_Nucleus_accumbens_basal_ganglia" "Brain_Putamen_basal_ganglia" "Brain_Spinal_cord_cervical_c-1" "Brain_Substantia_nigra" "Breast_Mammary_Tissue" "Cells_Cultured_fibroblasts" "Cells_EBV-transformed_lymphocytes" "Colon_Sigmoid" "Colon_Transverse" "Esophagus_Gastroesophageal_Junction" "Esophagus_Mucosa" "Esophagus_Muscularis" "Heart_Atrial_Appendage" "Heart_Left_Ventricle" "Liver" "Lung" "Minor_Salivary_Gland" "Muscle_Skeletal" "Nerve_Tibial" "Ovary" "Pancreas" "Pituitary" "Prostate" "Skin_Not_Sun_Exposed_Suprapubic" "Skin_Sun_Exposed_Lower_leg" "Small_Intestine_Terminal_Ileum" "Spleen" "Stomach" "Testis" "Thyroid" "Uterus" "Vagina" "Whole_Blood")
-
 for chr in {1..22}
   for tiss in ${tissues[@]}
     do
     Rscript /homes/amulford/fusion_twas-master/FUSION.assoc_test.R \
     --sumstats /homes/amulford/sum_stats_b37/uc_anderson_2011_21297633_uc_efo0000729_1_gwas.sumstats.tsv.gz \
-    --weights /homes/amulford/fusion_weights/GTEx.Whole_Blood.pos \
+    --weights /homes/amulford/fusion_weights/GTEx.${tiss}.pos \
     --weights_dir /homes/amulford/fusion_weights/ \
     --ref_ld_chr /homes/amulford/fusion_twas-master/LDREF/1000G.EUR. \
     --chr ${chr} \
