85b316a1bef77a2cf60b415b0d6c253de229ae97
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2021-04-14 19:09:21 -0500

    Update format_fusion_sum_stats_input.sh

--- format_fusion_sum_stats_input.sh
+++ format_fusion_sum_stats_input.sh
@@ -3,7 +3,9 @@ awk '{print $3 FS $4 FS $5 FS $7/($8+0.001)}' uc_anderson_2011_21297633_uc_efo00
 #must have column names: SNP, A1, A2, Z
 
 #chol
+awk '{print $1 FS $5 FS $4 FS $7/($8+0.001)}' Prins_28887542_chol > Prins_28887542_chol_fusion.txt
 
 #asthma
+awk '{print $1 FS $4 FS $5 FS $8/($9+0.001)}' Shrine_30552067_moderate-severe_asthma.txt > Shrine_30552067_moderate-severe_asthma_fusion.txt
 
 #covid-19
