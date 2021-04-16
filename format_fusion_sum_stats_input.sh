bef76a407c81693f0e7593d888db1fb569e7573c
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2021-04-16 13:59:36 -0500

    Update format_fusion_sum_stats_input.sh

--- format_fusion_sum_stats_input.sh
+++ format_fusion_sum_stats_input.sh
@@ -1,6 +1,8 @@
 #uc gwas sum stats to fusion input
-awk '{print $3 FS $4 FS $5 FS $7/($8+0.001)}' uc_anderson_2011_21297633_uc_efo0000729_1_gwas.sumstats.tsv > uc_fusion_anderson_2011_21297633_uc_efo0000729_1_gwas.sumstats.tsv
-#must have column names: SNP, A1, A2, Z
+awk '{print $1 FS $3 FS $4 FS $5 FS $7/($8+0.001)}' uc_anderson_2011_21297633_uc_efo0000729_1_gwas.sumstats.tsv > uc_fusion_anderson_2011_21297633_uc_efo0000729_1_gwas.sumstats.tsv
+#split by chr
+grep "1 rs" uc_fusion_anderson_2011_21297633_uc_efo0000729_1_gwas.sumstats.tsv > uc_fusion_anderson_2011_21297633_efo0000729_gwas.sumstats_1_rs.tsv
+#add column names: CHR SNP A1 A2 Z
 
 #chol
 awk '{print $2 FS $1 FS $5 FS $4 FS $7/($8+0.001)}' Prins_28887542_chol > Prins_28887542_chol_fusion.txt
@@ -14,4 +16,3 @@ awk '{print $2 FS $1 FS $4 FS $5 FS $8/($9+0.001)}' Shrine_30552067_moderate-sev
 grep "1 rs" Shrine_30552067_moderate-severe_asthma_fusion.txt > Shrine_30552067_moderate-severe_asthma_fusion_1_rs.txt
 #add column names: CHR SNP A1 A2 Z
 
-#covid-19
