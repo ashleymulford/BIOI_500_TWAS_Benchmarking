d9cc3867a43acea9cbaab8e4266eaaa6ff459175
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2021-04-20 21:58:20 -0500

    Update fusion.sh

--- fusion.sh
+++ fusion.sh
@@ -37,7 +37,7 @@ done
 declare -a tissues=("Adipose_Subcutaneous" "Adipose_Visceral_Omentum" "Adrenal_Gland" "Artery_Aorta" "Artery_Coronary" "Artery_Tibial" "Brain_Caudate_basal_ganglia" "Brain_Cerebellar_Hemisphere" "Brain_Cerebellum" "Brain_Cortex" "Brain_Frontal_Cortex_BA9" "Brain_Hippocampus" "Brain_Hypothalamus" "Brain_Nucleus_accumbens_basal_ganglia" "Brain_Putamen_basal_ganglia" "Breast_Mammary_Tissue" "Cells_Transformed_fibroblasts" "Cells_EBV-transformed_lymphocytes" "Colon_Sigmoid" "Colon_Transverse" "Esophagus_Gastroesophageal_Junction" "Esophagus_Mucosa" "Esophagus_Muscularis" "Heart_Atrial_Appendage" "Heart_Left_Ventricle" "Liver" "Lung" "Muscle_Skeletal" "Nerve_Tibial" "Ovary" "Pancreas" "Pituitary" "Prostate" "Skin_Not_Sun_Exposed_Suprapubic" "Skin_Sun_Exposed_Lower_leg" "Small_Intestine_Terminal_Ileum" "Spleen" "Stomach" "Testis" "Thyroid" "Uterus" "Vagina" "Whole_Blood")
 for tiss in ${tissues[@]}
   do
-  for chr in {1..22}
+  for chr in {1..2}
     do
     /usr/bin/time -v Rscript /homes/amulford/fusion_twas-master/FUSION.assoc_test.R \
     --sumstats /homes/amulford/sum_stats_b37/Shrine_30552067_moderate-severe_asthma_fusion_${chr}_rs.txt \
@@ -49,27 +49,10 @@ for tiss in ${tissues[@]}
   done
 done
 
-
-
-declare -a tissues=( "Adipose_Visceral_Omentum" "Artery_Aorta" "Artery_Tibial" "Brain_Cerebellar_Hemisphere" "Brain_Cortex" "Brain_Hippocampus")
+declare -a tissues=("Adipose_Subcutaneous" "Adipose_Visceral_Omentum" "Adrenal_Gland" "Artery_Aorta" "Artery_Coronary" "Artery_Tibial" "Brain_Caudate_basal_ganglia" "Brain_Cerebellar_Hemisphere" "Brain_Cerebellum" "Brain_Cortex" "Brain_Frontal_Cortex_BA9" "Brain_Hippocampus" "Brain_Hypothalamus" "Brain_Nucleus_accumbens_basal_ganglia" "Brain_Putamen_basal_ganglia" "Breast_Mammary_Tissue" "Cells_Transformed_fibroblasts" "Cells_EBV-transformed_lymphocytes" "Colon_Sigmoid" "Colon_Transverse" "Esophagus_Gastroesophageal_Junction" "Esophagus_Mucosa" "Esophagus_Muscularis" "Heart_Atrial_Appendage" "Heart_Left_Ventricle" "Liver" "Lung" "Muscle_Skeletal" "Nerve_Tibial" "Ovary" "Pancreas" "Pituitary" "Prostate" "Skin_Not_Sun_Exposed_Suprapubic" "Skin_Sun_Exposed_Lower_leg" "Small_Intestine_Terminal_Ileum" "Spleen" "Stomach" "Testis" "Thyroid" "Uterus" "Vagina" "Whole_Blood")
 for tiss in ${tissues[@]}
   do
-  for chr in {1..22}
-    do
-    /usr/bin/time -v Rscript /homes/amulford/fusion_twas-master/FUSION.assoc_test.R \
-    --sumstats /homes/amulford/sum_stats_b37/Shrine_30552067_moderate-severe_asthma_fusion_${chr}_rs.txt \
-    --weights /homes/amulford/fusion_weights/GTEx.${tiss}.pos \
-    --weights_dir /homes/amulford/fusion_weights/ \
-    --ref_ld_chr /homes/amulford/fusion_twas-master/LDREF/1000G.EUR. \
-    --chr ${chr} \
-    --out /homes/amulford/fusion_output/asthma_${tiss}_${chr}_assoc.txt
-  done
-done
-
-declare -a tissues=("Adrenal_Gland" "Artery_Coronary" "Brain_Caudate_basal_ganglia" "Brain_Cerebellum" "Brain_Frontal_Cortex_BA9" "Brain_Hypothalamus")
-for tiss in ${tissues[@]}
-  do
-  for chr in {1..22}
+  for chr in {3..4}
     do
     /usr/bin/time -v Rscript /homes/amulford/fusion_twas-master/FUSION.assoc_test.R \
     --sumstats /homes/amulford/sum_stats_b37/Shrine_30552067_moderate-severe_asthma_fusion_${chr}_rs.txt \
